6284aaee449ef18a9fa191afaa926328654ce8f0
Bruno <david.lopez9926@gmail.com>
2025-09-23 23:59:35 -0500

    adding error message for game list

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,28 +1,85 @@
+import { useEffect, useState } from 'react';
+
 import GameListItem from './GameListItem';
 //styles
-import styles from './GameList.module.scss'
+import styles from './GameList.module.scss';
+import { VscError } from "react-icons/vsc";
 // data
-import gameListData from '../../data/games.json';
-import type {Game} from '../../types/Game';
+import type { Game } from '../../types/Game';
 
-
-const gameList: Array<Game> = gameListData as Array<Game>;
+async function fetchGames(): Promise<Game[]> {
+  const res = await fetch('http://localhost:3000/games/');
+  const json = await res.json();
+  if (!Array.isArray(json)) throw new Error("Invalid response");
+  return json as Game[];
+}
 
 const GameList = () => {
+
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const loadGameList = async () => {
+    try {
+      setLoading(true);
+      const games: Game[] = await fetchGames();
+      setGames(games);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadGameList();
+  }, [])
+
   return (
     <section className={`${styles['game-list-section']} dashboard-container`}>
-        <h2>Game List</h2>
-        <div className={styles['game-list']}>
-            {gameList.map(game => (
-                <GameListItem
-                    key={game.id}
-                    name={game.name}
-                    status={game.status}
-                />
-            ))}
-        </div>
+      <h2>Game List</h2>
+      {
+        loading ? <Loading /> :
+          error ? <ErrorMessage message={error} retryAction={loadGameList} /> :
+            <List games={games} />
+      }
     </section>
   )
 }
+
+const List = ({ games }: { games: Game[] }) => (
+  <div className={styles['game-list']}>
+    {
+      games.map(game => (
+        <GameListItem
+          key={game.id}
+          name={game.name}
+          status={game.status}
+        />
+      ))}
+  </div>
+)
+
+const Loading = () => (
+  <span>Loading...</span>
+)
+
+const ErrorMessage = ({ message, retryAction }: { message: string, retryAction: () => void }) => (
+  <div className={styles['error-message']}>
+    <div className={styles['error-title']}>
+      <VscError />
+      <span>Error</span>
+    </div>
+    <div>
+      <span className={styles.error}>{message}, </span>
+      <button onClick={() => retryAction()}>
+        Retry
+      </button>
+    </div>
+  </div>
+)
 
 export default GameList;
