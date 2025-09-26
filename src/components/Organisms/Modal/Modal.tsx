eaee99aedabb7ded3e44ca2cf1e171e49c13b257
Bruno <david.lopez9926@gmail.com>
2025-09-25 19:14:12 -0500

    finishing modal

--- src/components/Organisms/Modal/Modal.tsx
+++ src/components/Organisms/Modal/Modal.tsx
@@ -3,6 +3,7 @@ import Button from '../../Atoms/Button/Button';
 import styles from './Modal.module.scss';
 import { IoClose } from "react-icons/io5";
 import { useEffect } from 'react';
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 
 interface ModalProps {
     title: string,
@@ -15,37 +16,59 @@ interface ModalProps {
 const Modal: FC<ModalProps> = ({ title, children, positiveAction, isOpen, close }) => {
 
     useEffect(() => {
-        document.body.classList.add("modal-open");
+        if (isOpen) {
+            document.body.classList.add("modal-open");
+        } else {
+            document.body.classList.remove("modal-open");
+        }
         return () => {
             document.body.classList.remove("modal-open");
         }
-    }, []);
+    }, [isOpen]);
 
     return (
-        <>
-        {
-            isOpen &&
-            <div className={styles.modal}>
-            <div className={styles['modal-card']}>
-                <div className={styles.header}>
-                    <h2>{title}</h2>
-                    <IoClose className={styles.closeButton} onClick={close} />
-                </div>
-                <div className={styles.body}>
-                    {children}
-                </div>
-                <div className={styles.footer}>
-                    <Button variant='secondary' onClick={close}>Close</Button>
-                    {
-                        positiveAction &&
-                        <Button onClick={() => positiveAction()}>Add</Button>
-                    }
-                </div>
-            </div>
-            <div className={styles.overlay} onClick={close}></div>
-        </div>
-        }
-        </>
+        <MotionConfig transition={{ type: "tween", duration: 0.2 }}>
+            <AnimatePresence>
+                {
+                    isOpen &&
+                    <motion.div
+                        className={styles.modal}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <motion.div
+                            className={styles['modal-card']}
+                            initial={{ y: 50, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1, transition: { delay: 0.1, duration: 0.3 } }}
+                            exit={{ y: 50 }}
+                        >
+                            <div className={styles.header}>
+                                <h2>{title}</h2>
+                                <IoClose className={styles.closeButton} onClick={close} />
+                            </div>
+                            <div className={styles.body}>
+                                {children}
+                            </div>
+                            <div className={styles.footer}>
+                                <Button variant='secondary' onClick={close}>Close</Button>
+                                {
+                                    positiveAction &&
+                                    <Button onClick={() => positiveAction()}>Add</Button>
+                                }
+                            </div>
+                        </motion.div>
+                        <motion.div
+                            className={styles.overlay}
+                            onClick={close}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                        />
+                    </motion.div>
+                }
+            </AnimatePresence>
+        </MotionConfig>
     )
 }
 
