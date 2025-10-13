c74e69f6dc5e76f73dc4718a07ba63073ef5bcf6
David Lopez <david.lopez@veevart.com>
2025-10-13 15:50:18 -0500

    theme toggle fix

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,31 +1,30 @@
-import { useState } from 'react';
-import styles from './ThemeToggle.module.scss';
+import { useState } from "react";
+import styles from "./ThemeToggle.module.scss";
 
 import { IoSunny, IoMoon } from "react-icons/io5";
 
-
 const ThemeToggle = () => {
+  const [mode, setMode] = useState("dark");
 
-    const [mode, setMode] = useState('dark')
+  const light = "light";
 
-    const light = 'light'
+  const toggleMode = () => {
+    setMode((prevMode) => {
+      const newMode = prevMode === "dark" ? light : "dark";
+      document.body.className = newMode;
+      return newMode;
+    });
+  };
 
-    const toggleMode = () => {
-        setMode((prevMode) => (prevMode === 'dark' ? light : 'dark'))
-        document.body.className = mode === 'dark' ? light : 'dark'
-    }
+  return (
+    <button
+      className={`${styles["theme-toggle"]} ${styles[mode]}`}
+      onClick={() => toggleMode()}
+      title={`change to ${mode === "dark" ? light : "dark"} theme`}
+    >
+      {mode === "dark" ? <IoMoon /> : <IoSunny />}
+    </button>
+  );
+};
 
-    return (
-        <button
-            className={`${styles['theme-toggle']} ${styles[mode]}`}
-            onClick={() => toggleMode()}
-            title={`change to ${mode === 'dark' ? light : 'dark'} theme`}
-            >
-            {
-                mode === 'dark' ? <IoMoon/> : <IoSunny />
-            }
-        </button>
-    )
-}
-
-export default ThemeToggle
+export default ThemeToggle;
