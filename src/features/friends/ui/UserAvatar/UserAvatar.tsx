06a8b0abaec17c64af10c33dad2f0d3c1c20d2a8
Bruno <david.lopez9926@gmail.com>
2026-05-01 19:14:49 -0500

    changing the avatar color for each user

--- src/features/friends/ui/UserAvatar/UserAvatar.tsx
+++ src/features/friends/ui/UserAvatar/UserAvatar.tsx
@@ -1,3 +1,17 @@
+const avatarColors = [
+    'bg-avatar-1',
+    'bg-avatar-2',
+    'bg-avatar-3',
+    'bg-avatar-4',
+    'bg-avatar-5',
+    'bg-avatar-6',
+];
+
+const getAvatarColor = (name: string) => {
+    const hash = name.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    return avatarColors[hash % avatarColors.length];
+};
+
 type UserAvatarProps = {
     name: string;
     avatarUrl?: string;
@@ -10,7 +24,7 @@ const UserAvatar = ({ name, avatarUrl }: UserAvatarProps) => {
         <div className="rounded-xl w-12 aspect-square overflow-hidden shrink-0">
             {avatarUrl
                 ? <img className="w-full h-full object-cover" src={avatarUrl} alt={`${name}'s avatar`} />
-                : <div className="flex justify-center items-center bg-primary w-full h-full font-bold text-primary-foreground">{initials}</div>
+                : <div className={`flex justify-center items-center w-full h-full font-bold text-white ${getAvatarColor(name)}`}>{initials}</div>
             }
         </div>
     );
