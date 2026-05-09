e5d53f883b2d11ac085ed408ef68f843b6f58029
Bruno <david.lopez9926@gmail.com>
2026-05-09 16:40:20 -0500

    using and configuring VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,36 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GameTracker',
+        short_name: 'GameTracker',
+        theme_color: '#cc4e45',
+        background_color: '#121212',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      }
+    }),
+  ],
   server: {
     allowedHosts: ['.ngrok-free.app']
   },
