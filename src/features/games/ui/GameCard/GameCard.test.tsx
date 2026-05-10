13a4dd989ab8e292c992d5e6d789467908ca19c9
Bruno <david.lopez9926@gmail.com>
2026-05-09 21:50:50 -0500

    more tests

--- src/features/games/ui/GameCard/GameCard.test.tsx
+++ src/features/games/ui/GameCard/GameCard.test.tsx
@@ -43,4 +43,12 @@ describe("GameCard", () => {
         const image = screen.getByRole("img", { name: new RegExp(game.name, 'i') });
         expect(image).toHaveAttribute("src", "/games/default-cover.jpg");
     })
+
+    it("renders provided actions", () => {
+        const game = gameFactory({ coverUrl: undefined });
+        render(<GameCard name={game.name} status={game.status} coverUrl={game.coverUrl} actions={<button>Action</button>} />);
+
+        const button = screen.getByRole("button", { name: "Action" });
+        expect(button).toBeInTheDocument();
+    })
 });
